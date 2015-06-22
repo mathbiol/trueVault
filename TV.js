c63995a0f090f9d63715dbc4ec0aa46445636f0e
Jonas Almeida <jalmeida@mathbiol.org>
2015-06-22 12:11:58 -0400

    login via localStorage for experimentation

--- TV.js
+++ TV.js
@@ -1,1 +1,15 @@
-console.log('loaded featureSpace.js')
+console.log('loaded TV.js')
+
+TV=function(){
+    var conn = localStorage.getItem('trueVaultConnect')
+    if(!conn){
+        console.log("localStorage.setItem('trueVaultConnect','{\"user\":\"XXX\",\"apiKey\":\"XXXXXX\"}')")
+    }else{
+        this.conn=JSON.parse(conn)
+        console.log(this.conn)
+    }
+} // ini
+
+
+
+TV()
